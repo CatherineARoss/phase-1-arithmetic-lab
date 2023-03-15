a39c1285411ec1d701ae75422e1e1d66007209f7
Catherine Ross <fibervibes@gmail.com>
2023-03-15 17:17:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 =31; 
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 1;
+Math.random(random);
+
+let num3 = 89;
+let num4 =  5;
+let mod = num3 % num4;
+
+let max = (3,8,13,20);
+Math.max(max);
